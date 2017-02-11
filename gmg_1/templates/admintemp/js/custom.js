495f04f516b0a772d14143866243261199c83d8c
gautham31 <gauthamgajith@yahoo.c.in>
2017-02-11 17:08:10 -0500

    added admin templates

---
+++ gmg_1/templates/admintemp/js/custom.js
@@ -0,0 +1,65 @@
+var allLinks = document.links;
+allLinks[0].onclick=function(){
+    $('.tabContent').css({
+        'display':'none'
+    });
+    $('#tab0').css({
+        'display':'block'
+    });
+}
+allLinks[1].onclick=function(){
+    $('.tabContent').css({
+        'display':'none'
+    });
+    $('#tab1').css({
+        'display':'block'
+    });
+}
+allLinks[2].onclick=function(){
+    $('.tabContent').css({
+        'display':'none'
+    });
+    $('#tab2').css({
+        'display':'block'
+    });
+}
+allLinks[3].onclick=function(){
+    $('.tabContent').css({
+        'display':'none'
+    });
+    $('#tab3').css({
+        'display':'block'
+    });
+}   
+allLinks[4].onclick=function(){
+    $('.tabContent').css({
+        'display':'none'
+    });
+    $('#tab4').css({
+        'display':'block'
+    });
+}
+allLinks[5].onclick=function(){
+    $('.tabContent').css({
+        'display':'none'
+    });
+    $('#tab5').css({
+        'display':'block'
+    });
+}
+allLinks[6].onclick=function(){
+    $('.tabContent').css({
+        'display':'none'
+    });
+    $('#tab6').css({
+        'display':'block'
+    });
+}
+allLinks[7].onclick=function(){
+    $('.tabContent').css({
+        'display':'none'
+    });
+    $('#tab7').css({
+        'display':'block'
+    });
+}
