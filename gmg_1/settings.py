8da3a0f582676bc7b7efb4450c8251695ce78db0
gauthamgajith <gauthamgajith@yahoo.co.in>
2017-08-25 21:35:38 -0400

    bug fix

--- gmg_1/settings.py
+++ gmg_1/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '22ook^k5*itv9z@=5si=9#r!xz4@zgy^mwmu64z-komdg_6xhf'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
